d4a0baaff1e93d15a54dd43b5d1717f08ad0a3a1
teacher <teacher@itstep.com>
2015-04-20 21:06:51 +0300

    add email

--- src/com/example/testCrash/Const.java
+++ src/com/example/testCrash/Const.java
@@ -2,6 +2,6 @@ package com.example.testCrash;
 
 public class Const {
 
-    public static String CRASH_REPORTS_EMAILS = "test@test.test";
+    public static String[] CRASH_REPORTS_EMAILS = new String[]{"test1@test.test", "test2@test.test", "test3@test.test"};
 
 }
